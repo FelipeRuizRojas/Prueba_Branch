6ae81df49f9abc5b0cbf100a196e10c4bfd2cd9f
FelipeRuizRojas <feliruizrojas29@gmail.com>
2026-03-22 20:47:17 -0600

    commit 2

--- src/main/java/com/example/PruebaBranch/PruebaBranch/PruebaBranchApplication.java
+++ src/main/java/com/example/PruebaBranch/PruebaBranch/PruebaBranchApplication.java
@@ -2,7 +2,7 @@ package com.example.PruebaBranch.PruebaBranch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Version 1
+//Version 2
 @SpringBootApplication
 public class PruebaBranchApplication {
 
