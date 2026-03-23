7324b4bdb15d5bde2abceb4a7cfbb9dbacfb77f7
FelipeRuizRojas <feliruizrojas29@gmail.com>
2026-03-22 20:39:28 -0600

    commit 1 main

---
+++ src/main/java/com/example/PruebaBranch/PruebaBranch/PruebaBranchApplication.java
@@ -0,0 +1,13 @@
+package com.example.PruebaBranch.PruebaBranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Version 1
+@SpringBootApplication
+public class PruebaBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBranchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PruebaBranch/PruebaBranch/PruebaBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PruebaBranch.PruebaBranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
